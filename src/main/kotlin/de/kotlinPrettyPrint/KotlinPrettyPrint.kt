e62555f57f1cb033a497abb7cf15508266acadfd
m-moog <maltemoog@gmail.com>
2022-04-26 21:55:58 +0200

    added KDoc

--- src/main/kotlin/de/kotlinPrettyPrint/KotlinPrettyPrint.kt
+++ src/main/kotlin/de/kotlinPrettyPrint/KotlinPrettyPrint.kt
@@ -1,5 +1,43 @@
 package de.kotlinPrettyPrint
 
+/**
+ * Allows for easy and readable printing of complex data structures
+ *
+ * Inside the lambda expression, the [InsidePrettyPrint.printForEach] function becomes available.
+ * This function will apply a template to each Element of an Iterable<>.
+ *
+ * ### Example function call
+ * ```kotlin
+ * prettyPrint {
+ *     """
+ *         List of Programmers:
+ *         ${employees.printForEach {
+ *         """
+ *             $firstName $lastName
+ *             Age: $age
+ *             Programming Languages: $programmingLanguages
+ *         """
+ *         }}
+ *     """
+ * }
+ * ```
+ * ### Example Output:
+ * ```text
+ * List of Programmers:
+ *
+ *  Hans Günther
+ *  Age: 42
+ *  Programming Languages: [Kotlin, java]
+ *
+ *  Jane Smith
+ *  Age: 27
+ *  Programming Languages: [Kotlin, Javascript]
+ * ```
+ *
+ * For further explanation and examples see [GitHub](https://github.com/m-moog/kotlin-pretty-print)
+ *
+ * @author M. Moog - 26.04.2022
+ */
 fun prettyPrint(func: InsidePrettyPrint.() -> String): String{
     return func.invoke(InsidePrettyPrintImpl()).trimIndent()
 }
